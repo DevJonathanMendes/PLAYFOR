13da2f22cff94231e0df9f8338391bfc8631a1bf
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-12-03 17:11:27 -0300

    feat: Authentication

--- BACK/src/app.module.ts
+++ BACK/src/app.module.ts
@@ -4,9 +4,11 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
 	imports: [
@@ -16,6 +18,8 @@ import { UserService } from './user/user.service';
 		}),
 		ConfigModule.forRoot(),
 		UserModule,
+		AuthModule,
+		UsersModule,
 	],
 	controllers: [AppController],
 	providers: [PrismaService, UserService],
