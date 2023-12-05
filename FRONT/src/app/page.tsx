56f9f8fd4b395eb4dc24645005d8ff4630554b46
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-12-05 18:50:09 -0300

    feat: Add react-hook-form, zod, axios etc

--- FRONT/src/app/page.tsx
+++ FRONT/src/app/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+import axiosLogin from "@/api/axios";
+import { LoginSchemaData, loginSchema } from "@/zodSchema/userSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
-import Image from "next/image";
+import { SubmitHandler, useForm } from "react-hook-form";
+import Label from "./components/Label";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<LoginSchemaData>({ resolver: zodResolver(loginSchema) });
+
+  const onSubmit: SubmitHandler<LoginSchemaData> = async (data) => {
+    const res = await axiosLogin({ ...data });
+
+    if (!res.data) {
+      return setError("root", {
+        type: "manual",
+        message: "The account does not exist or is incorrect.",
+      });
+    }
+
+    // Precisa estar sempre validando autenticação.
+    router.push("/chat");
+  };
+
   return (
     <main className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -10,71 +39,37 @@ export default function Home() {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Login in to your account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form
+              className="space-y-4 md:space-y-6"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              {errors.root && (
+                <span className="text-black">{errors.root.message}</span>
+              )}
               <div>
-                <label
-                  htmlFor="username"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Username
-                </label>
+                <Label htmlFor="username" content="Username" />
                 <input
-                  type="username"
-                  name="username"
-                  id="username"
+                  {...register("username", { required: true })}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Username"
-                  required
+                />
+                {errors.username && (
+                  <span className="text-black">{errors.username.message}</span>
+                )}
+              </div>
+              <div>
+                <Label htmlFor="password" content="Password" />
+                <input
+                  {...register("password", { required: true })}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
               </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required
-                />
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    />
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label
-                      htmlFor="remember"
-                      className="text-gray-500 dark:text-gray-300"
-                    >
-                      Remember me
-                    </label>
-                  </div>
-                </div>
-                <a
-                  href="#"
-                  className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
-              <button
+              {errors.password && (
+                <span className="text-black">{errors.password.message}</span>
+              )}
+              <input
                 type="submit"
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Login
-              </button>
+              />
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 {"Don't have an account? "}
                 <Link
