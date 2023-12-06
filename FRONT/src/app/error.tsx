ff2db622b65b8cc985582bd1ca960b066d47b55b
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-12-06 18:48:24 -0300

    New error page

--- FRONT/src/app/error.tsx
+++ FRONT/src/app/error.tsx
@@ -1,21 +1,39 @@
 "use client";
+import Link from "next/link";
 import { useEffect } from "react";
 
 export default function Error({
   error,
-  reset,
 }: {
   error: Error & { digest?: string };
-  reset: () => void;
 }) {
   useEffect(() => {
     console.error(error);
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button onClick={() => reset()}>Try again</button>
-    </div>
+    <section className="relative z-10 bg-primary py-[120px]">
+      <div className="container mx-auto">
+        <div className="-mx-4 flex">
+          <div className="w-full px-4">
+            <div className="mx-auto max-w-[400px] text-center">
+              <h2 className="mb-2 text-[50px] font-bold leading-none text-white sm:text-[80px] md:text-[100px]">
+                Error
+              </h2>
+              <h4 className="mb-3 text-[22px] font-semibold leading-tight text-white">
+                Oops!
+              </h4>
+              <p className="mb-8 text-lg text-white">Something went wrong</p>
+              <Link
+                href="/"
+                className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white transition hover:bg-slate-500 hover:text-primary"
+              >
+                Go To Login
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
