549688a251dbf7bb433a9a38667f3b6215f786e1
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-12-19 22:05:51 -0300

    Initial handler error class-validator

--- BACK/src/user/user.filter.ts
+++ BACK/src/user/user.filter.ts
@@ -1,13 +1,15 @@
-import { Catch, HttpException } from '@nestjs/common';
+import { BadRequestException, Catch, HttpException } from '@nestjs/common';
 import { GqlExceptionFilter } from '@nestjs/graphql';
 import { Prisma } from '@prisma/client';
 import { GraphQLError } from 'graphql';
 
 const { PrismaClientKnownRequestError: PrismaError } = Prisma;
 
-@Catch(GraphQLError, PrismaError)
+@Catch()
 export class UserExceptionFilter implements GqlExceptionFilter {
 	catch(err: HttpException) {
+		console.log(err);
+
 		if (err instanceof PrismaError && err.code === 'P2002') {
 			throw new GraphQLError(
 				`Unique constraint failed on the ${err.meta.target}`,
@@ -17,6 +19,10 @@ export class UserExceptionFilter implements GqlExceptionFilter {
 					},
 				},
 			);
+		}
+
+		if (err instanceof BadRequestException) {
+			throw new GraphQLError('Check that you have provided the right values');
 		}
 
 		throw new GraphQLError('Something went wrong, please try again later.');
