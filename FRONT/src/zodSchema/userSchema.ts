58142eed7cbb0c595b2933b5b64537b0c17dcb49
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-12-28 15:14:58 -0300

    refactor

--- FRONT/src/zodSchema/userSchema.ts
+++ FRONT/src/zodSchema/userSchema.ts
@@ -1,14 +1,14 @@
 import { z as zod } from "zod";
 
 export const loginSchema = zod.object({
-  username: zod.string().nonempty("Username is required"),
-  password: zod.string().nonempty("Password is required"),
+  username: zod.string().min(1, "Username is required"),
+  password: zod.string().min(1, "Password is required"),
 });
 
 export const registerSchema = loginSchema
   .extend({
-    email: zod.string().email(),
-    confirm_password: zod.string().nonempty("Confirm password is required"),
+    email: zod.string().email("Email is required"),
+    confirm_password: zod.string().min(1, "Confirm password is required"),
   })
   .refine(({ password, confirm_password }) => password === confirm_password, {
     message: "Password doesn't match",
